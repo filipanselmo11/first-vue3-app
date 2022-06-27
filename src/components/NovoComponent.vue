9c68eafb32c090eee9e253f4c5f7b0daeb34018c
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-27 15:04:09 -0400

    First vue 3 app (usando Vite)

---
+++ src/components/NovoComponent.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h2>Filter LearnVue Articles</h2>
+    <input type="text" placeholder="Filter Search" v-model="query" />
+    <br />
+    <br />
+    <button @click="reset">Reset</button>
+    <search-results :query="query"></search-results>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import SearchResults from "./SearchResults.vue";
+export default {
+  components: { SearchResults },
+  setup() {
+    const query = ref("");
+    const reset = (e) => {
+      query.value = "";
+    };
+    return {
+      query,
+      reset,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.root {
+  width: 400px;
+  margin: 0 auto;
+}
+.root p {
+  text-align: right;
+  font-size: 0.7em;
+  margin: 0;
+}
+ul {
+  list-style: none;
+  width: 50px 0;
+  padding: 0;
+  background-color: #fafafa;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+}
+li {
+  text-align: left;
+  padding: 20px;
+  border-bottom: 1px solid #ddd;
+}
+li:nth-last-of-type(1) {
+  border-bottom: none;
+}
+</style>
---
+++ src/components/SearchResults.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="root">
+    <p>Showing {{ filteredTitles.length }} results for "{{ query }}"</p>
+    <ul>
+      <li v-for="title in filteredTitles" :key="title.page">
+        {{ title.Name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { computed, onMounted } from "vue";
+import titles from "../post-data.json";
+export default {
+  props: {
+    query: String,
+  },
+  setup(props, context) {
+    const filteredTitles = computed(() => {
+      return titles.filter((s) =>
+        s.Name.toLowerCase().includes(props.query.toLowerCase())
+      );
+    });
+
+    return {
+      filteredTitles,
+    };
+    onMounted(() => {
+      console.log("Mounted");
+    });
+  },
+};
+</script>
+
+<style></style>
